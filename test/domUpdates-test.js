ffdec4d2bb2b6f89554bfcc06423b7f225d30c50
Alyssa Lundgren <lundgrea@gmail.com>
2019-07-25 15:23:53 -0600

    Add rough class files and associated test files

---
+++ test/domUpdates-test.js
@@ -0,0 +1,26 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+// import Player from '../src/Player';
+
+// var xx
+
+beforeEach(() => {
+ 
+});
+
+describe('domUpdates', function() {
+
+  // it('should be a function', function() {
+  //   expect(Player).to.be.a('function');
+  // });
+
+  // it('should have a name', function() {
+  //   expect(player1.name).to.equal('John')
+  // });
+
+  // it('should start with a score of 100', function() {
+  //   expect(player1.score).to.equal(100)
+  // })
+
+});
