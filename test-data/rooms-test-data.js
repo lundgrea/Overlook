051e080be296cb9cd103fc46da1c739443f8dee5
Alyssa Lundgren <lundgrea@gmail.com>
2019-07-25 08:49:38 -0600

    Add test data files and test data

---
+++ test-data/rooms-test-data.js
@@ -0,0 +1,36 @@
+{
+  rooms: [
+    {
+    number: 1,
+    roomType: "residential suite",
+    bidet: false,
+    bedSize: "twin",
+    numBeds: 1,
+    costPerNight: 265.03
+    },
+    {
+    number: 2,
+    roomType: "single room",
+    bidet: true,
+    bedSize: "full",
+    numBeds: 1,
+    costPerNight: 228.01
+    },
+    {
+    number: 3,
+    roomType: "suite",
+    bidet: false,
+    bedSize: "twin",
+    numBeds: 1,
+    costPerNight: 275.99
+    },
+    {
+    number: 4,
+    roomType: "junior suite",
+    bidet: false,
+    bedSize: "full",
+    numBeds: 1,
+    costPerNight: 177.03
+    }
+  ]
+}
