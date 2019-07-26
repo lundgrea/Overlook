87a6a08c855f2191c242b8ded948a990c943cc78
Alyssa Lundgren <lundgrea@gmail.com>
2019-07-26 07:48:50 -0600

    Integreate search, add, bed and room service svgs into comp

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import domUpdates from './domUpdates.js';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './images/search.svg'
+import './images/add.svg'
+import './images/roomservice.svg'
+import './images/bed.svg'
+
+
 
 $(document).ready(() => {
   $('#aside__date').text(domUpdates.displayDate())
