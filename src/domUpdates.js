93b97324bb71c5581b5880967b8cd5e2604afad2
Alyssa Lundgren <lundgrea@gmail.com>
2019-07-31 07:54:47 -0600

    add book now checkbox to bookings table

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -244,6 +244,7 @@ let domUpdates = {
             <td class="main__rooms-chart-line" id="main__rooms-chart-room-cost">${room.costPerNight}</td>
             <td class="main__rooms-chart-line" id="main__rooms-chart-room-bidet">${room.bidet}</td>
             <td class="main__rooms-chart-line" id="main__rooms-chart-room-availability">Yes</td>
+            <td class="main__rooms-chart-line" id="main__rooms-chart-room-availability"><input class="checkbox" type="checkbox" name="available" value="available" class="input__booking-checkmark" </input> </td>
           </tr>
         </table>`)
     })
